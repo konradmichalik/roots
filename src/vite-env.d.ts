448aed52318a0dfb710dd3fae8597341b678f5c0
Konrad Michalik <km@move-elevator.de>
2026-01-30 19:44:20 +0100

    feat: add app entry point and global styles

---
+++ src/vite-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+
+declare const __APP_VERSION__: string;
+declare const __APP_NAME__: string;
+declare const __BUILD_DATE__: string;
